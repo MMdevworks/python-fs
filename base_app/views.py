55f565835a934d359c4b14e89c99909f50eec132
Marie Marcos <mmdevq@gmail.com>
2024-03-12 11:38:04 -0700

    app setup

--- base_app/views.py
+++ base_app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
 
-def room(request):
-    return render(request, 'room.html')
+rooms = [
+    {'id': 1, 'name': 'Strength in Numbers'},
+    {'id': 2, 'name': 'Weekend Warriors'},
+    {'id': 3, 'name': 'Challenge Accepted'},
+]
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base_app/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base_app/room.html', context)
