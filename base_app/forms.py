7e830caade620d77ad0f7aef062d8f43a1db9542
Marie Marcos <mmdevq@gmail.com>
2024-03-18 10:27:12 -0700

    forms

--- base_app/forms.py
+++ base_app/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__' # will create form based on the metadata in models.py
         exclude = ['host', 'participants'] #excluded fields for form
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
